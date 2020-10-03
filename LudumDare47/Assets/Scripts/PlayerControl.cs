7623f6074356578ec89952bd0f52696eef072d53
VaustXIII <vaustXIII@gmail.com>
2020-10-03 16:34:27 +0300

    add basic player controller

---
+++ LudumDare47/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerControl : MonoBehaviour
+{
+    [SerializeField] private Rigidbody rigidbody;
+    [SerializeField] private float maxSpeed = 5f;
+
+    void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        SetVelocity();
+    }
+
+    private void SetVelocity() {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        var velocity = new Vector3(horizontal, 0f, vertical).normalized * maxSpeed;
+        rigidbody.velocity = velocity;
+    }
+}
