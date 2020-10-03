57c34b8ef68c0779489884aadda8b6c9b3a8eca7
VaustXIII <vaustXIII@gmail.com>
2020-10-03 17:36:50 +0300

    midwork commit

---
+++ LudumDare47/Assets/Scripts/TrainCarLooper.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainCarLooper : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> trainCarPrefabs;
+
+    private List<TrainCar> trainCars;
+    private TrainCar leftTrainCar;
+    private TrainCar rightTrainCar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitializeTrainCars();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void InitializeTrainCars() {
+
+    }
+}
