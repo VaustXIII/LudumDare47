85e6da9e037211fa8c7222b314c29cf044dbf320
VaustXIII <vaustXIII@gmail.com>
2020-10-03 19:08:05 +0300

    add follow script to camera

---
+++ LudumDare47/Assets/Scripts/Follow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    [SerializeField] Transform target;
+
+    private Vector3 fixedDistance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fixedDistance = transform.position - target.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = target.transform.position + fixedDistance;
+    }
+}
