c2694e64ab9691e3542ddb160f99a746112e5712
VaustXIII <vaustXIII@gmail.com>
2020-10-05 21:48:47 +0300

    add win

---
+++ LudumDare47/Assets/Scripts/QuestLast.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class QuestLast : MonoBehaviour
+{
+    [SerializeField] private Interactible machinistDialogueInteractible;
+
+    [SerializeField] private DialogueLines machinistLines;
+
+    private int linesRead = 0;
+
+    private void Start()
+    {
+        machinistDialogueInteractible.PlayerInteracted += OnPlayerTalkedToMachinist;
+    }
+
+    private void OnPlayerTalkedToMachinist()
+    {
+        linesRead++;
+        if (linesRead > machinistLines.Lines.Length) {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
