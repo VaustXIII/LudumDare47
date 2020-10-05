212a7050565f32c33381870071deafaef94adb63
VaustXIII <vaustXIII@gmail.com>
2020-10-05 10:01:12 +0300

    Setup hero items

--- LudumDare47/Assets/Scripts/PlayerControl.cs
+++ LudumDare47/Assets/Scripts/PlayerControl.cs
@@ -2,20 +2,68 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum SuitCaseType {
+    Red = 0,
+    Green,
+    Bluee
+}
+
+public enum ClothesType {
+    Tuxedo = 0,
+    Casual,
+    Barman
+}
+
+public enum DrinkType {
+    Drink1 = 0,
+    Drink2,
+    Drink3
+}
+
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerControl : MonoBehaviour
 {
+    public static PlayerControl Instance {get; private set;}
     [SerializeField] private Rigidbody rb;
     [SerializeField] private float maxSpeed = 5f;
 
+    [SerializeField] GameObject[] suitCases;
+    [SerializeField] GameObject[] clothes;
+    [SerializeField] GameObject[] drinks;
+
     void Awake()
     {
+        Instance = this;
         rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
         SetVelocity();
+    }
+
+    public void EquipSuitCase(SuitCaseType suitCase) {
+        foreach (var item in suitCases)
+        {
+            item.SetActive(false);
+        }
+        suitCases[(int)suitCase].SetActive(true);
+    }
+
+    public void EquipClothes(ClothesType clothesType) {
+        foreach (var item in clothes)
+        {
+            item.SetActive(false);
+        }
+        clothes[(int)clothesType].SetActive(true);
+    }
+
+    public void EquipDrink(DrinkType drink) {
+        foreach (var item in drinks)
+        {
+            item.SetActive(false);
+        }
+        drinks[(int)drink].SetActive(true);
     }
 
     private void SetVelocity() {
